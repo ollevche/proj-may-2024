a5cb877727b939f586495d131fa32af8dea41b2e
ollevche <ollevche@gmail.com>
2024-06-10 21:08:10 +0300

    add simple http api

--- webinar-09/main.go
+++ webinar-09/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // TODO:
@@ -16,9 +18,58 @@ func main() {
 		fmt.Println("In /hello")
 	})
 
+	mux.HandleFunc("/note", processNote)
+
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println("Error happenned: ", err.Error())
+		return
+	}
+}
+
+// Do not use global vars.
+var note = Note{
+	Value:     "Some initial val",
+	UpdatedAt: time.Now(),
+}
+
+func processNote(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getNote(w, r)
+	case http.MethodPut:
+		putNote(w, r)
+	}
+}
+
+type Note struct {
+	Value     string
+	UpdatedAt time.Time
+}
+
+func getNote(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(note)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func putNote(w http.ResponseWriter, r *http.Request) {
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	note.UpdatedAt = time.Now()
+
+	err = json.NewEncoder(w).Encode(note)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
