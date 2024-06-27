7c0e09e14c365197acbe53e823667009290849ff
ollevche <ollevche@gmail.com>
2024-06-27 21:07:07 +0300

    add webinar 14

---
+++ webinar-14/go.mod
@@ -0,0 +1,5 @@
+module web14
+
+go 1.22.3
+
+require github.com/ollevche/calculator v0.0.0-20240627180216-7f9047f01d6b
---
+++ webinar-14/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ollevche/calculator/operation"
+)
+
+func main() {
+	fmt.Println(operation.Add(1, 2))
+}
